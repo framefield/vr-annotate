5abb8827de8bd4e870abd88b63ee2d00292e992f
client.smartkitchen <client.smartkitchen@framefield.com>
2017-05-16 14:35:39 +0200

    Added first stub ob annotatableGroup and Node

---
+++ Assets/_project/ff.vr.annotation/AnnotatableGroup.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ff.vr.annotation;
+
+namespace ff.vr.annotation
+{
+    public class AnnotatableGroup : MonoBehaviour
+    {
+        public string DB_ReferenceID;
+
+        private void Start()
+        {
+        }
+
+        private void Awake()
+        {
+            CreateNodesFromHierachy();
+        }
+
+        private void CreateNodesFromHierachy()
+        {
+            var node = AddObjectToNode(this.gameObject);
+            node.PrintStructure();
+        }
+
+
+        private AnnotatableNode AddObjectToNode(GameObject unityObj)
+        {
+            var newNode = new AnnotatableNode()
+            {
+                IsAnnotatable = CheckIfObjectIsAnnotatable(unityObj),
+                Name = unityObj.name,
+                Children = new AnnotatableNode[unityObj.transform.childCount],
+            };
+
+            var renderer = unityObj.GetComponent<MeshRenderer>();
+            if (renderer)
+            {
+                newNode.Bounds = renderer.bounds;   // in worldspace
+                newNode.HasBounds = true;
+            }
+
+
+            for (int index = 0; index < unityObj.transform.childCount; index++)
+            {
+                var childObj = unityObj.transform.GetChild(index).gameObject;
+                var childNode = AddObjectToNode(childObj);
+                newNode.Children[index] = childNode;
+
+                if (childNode.HasBounds)
+                {
+                    if (newNode.HasBounds)
+                    {
+                        newNode.Bounds.Encapsulate(childNode.Bounds);
+                    }
+                    else
+                    {
+                        newNode.Bounds = childNode.Bounds;
+                    }
+                }
+            }
+            return newNode;
+        }
+
+
+        private bool CheckIfObjectIsAnnotatable(GameObject obj)
+        {
+            return true;
+        }
+    }
+}
---
+++ Assets/_project/ff.vr.annotation/AnnotatableNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ff.vr.annotation
+{
+    [System.Serializable]
+    public class AnnotatableNode
+    {
+        public AnnotatableNode Parent;
+        public AnnotatableNode[] Children;
+        public Bounds Bounds;
+        public bool IsAnnotatable;
+        public string Name;
+        public bool HasBounds = false;
+
+        [System.NonSerializedAttribute]
+        public GameObject ObjectReference;
+
+        public void PrintStructure(int level = 0)
+        {
+            Debug.LogFormat(new System.String(' ', level) + this.Name);
+            foreach (var c in Children)
+            {
+                c.PrintStructure(level + 1);
+            }
+        }
+    }
+}
