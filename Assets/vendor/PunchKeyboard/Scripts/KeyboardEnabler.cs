ddc2e3932ddeff96ebb176776ec356e6c31e110d
client.smartkitchen <client.smartkitchen@framefield.com>
2017-05-19 21:10:21 +0200

    Added first stub of virtual PunchKeyboard

---
+++ Assets/vendor/PunchKeyboard/Scripts/KeyboardEnabler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace ff.vr.interaction
+{
+    /* Switch to virtual keyboard mode if one of the controllers 
+    comes too close to to PunchKeyboard.
+    */
+    public class KeyboardEnabler : MonoBehaviour
+    {
+        public bool IsKeyboardEnabled = false;
+        public float ThresholdDistance = 0.6f;
+
+        public InteractiveController[] _controllers;
+        public KeyboardControllerStick[] _sticks;
+
+        [Header("--- internal prefab references -----")]
+        public GameObject PunchKeyboardObject;
+        public InputField _inputField;
+
+
+        void Start()
+        {
+            SetIsKeyboardEnabled(false, forceUpdate: true);
+        }
+
+        void Update()
+        {
+            if (_controllers == null)
+                return;
+
+            var tooClose = false;
+            foreach (var controller in _controllers)
+            {
+                var distance = Vector3.Distance(controller.transform.position, this.transform.position);
+                tooClose |= distance < ThresholdDistance;
+            }
+            SetIsKeyboardEnabled(tooClose);
+        }
+
+        private void SetIsKeyboardEnabled(bool newState, bool forceUpdate = true)
+        {
+            if (newState != IsKeyboardEnabled || forceUpdate)
+            {
+                IsKeyboardEnabled = newState;
+                PunchKeyboardObject.SetActive(newState);
+
+                foreach (var stick in _sticks)
+                {
+                    stick.gameObject.SetActive(IsKeyboardEnabled);
+                }
+
+                if (IsKeyboardEnabled)
+                {
+                    EventSystem.current.SetSelectedGameObject(_inputField.gameObject, null);
+                    //EventSystemManager.currentSystem.SetSelectedGameObject(_inputField.gameObject, null);
+                    //_inputField.OnPointerClick(null);
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/vendor/PunchKeyboard/Scripts/VR/CollisionFeedbackController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionFeedbackController : MonoBehaviour
+{
+    public SteamVR_TrackedObject TrackedObject;
+    private SteamVR_Controller.Device device;
+    private const int KeyPressFeedbackStrength = 1500;
+    private bool isColliding = false;
+
+    void Start()
+    {
+        if (TrackedObject == null)
+        {
+            TrackedObject = GetComponentInParent<SteamVR_TrackedObject>();
+        }
+    }
+
+    void OnEnable()
+    {
+        Key.keyPressed += KeyPressedHapticFeedback;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isColliding = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isColliding = false;
+    }
+
+    private void KeyPressedHapticFeedback()
+    {
+        if (isColliding)
+        {
+            StartCoroutine("TriggerHapticFeedback", KeyPressFeedbackStrength);
+        }
+    }
+
+    private void Update()
+    {
+        device = SteamVR_Controller.Input((int)TrackedObject.index);
+    }
+
+    private void OnDisable()
+    {
+        Key.keyPressed -= KeyPressedHapticFeedback;
+    }
+
+    private IEnumerator TriggerHapticFeedback(int strength)
+    {
+        device.TriggerHapticPulse(500);
+        yield return new WaitForEndOfFrame();
+        device.TriggerHapticPulse(2000);
+        yield return new WaitForEndOfFrame();
+    }
+}
