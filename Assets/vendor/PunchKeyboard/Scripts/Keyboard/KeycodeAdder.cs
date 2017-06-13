587b2f6f0ea4d044c2368a037ad73b15aa708ac4
pixtur <thomas.mann@framefield.com>
2017-06-13 15:35:27 +0200

    Added missing 3rdParty plugins

--- Assets/vendor/PunchKeyboard/Scripts/Keyboard/KeycodeAdder.cs
+++ Assets/vendor/PunchKeyboard/Scripts/Keyboard/KeycodeAdder.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using WindowsInput;
 
+
 public class KeycodeAdder : MonoBehaviour
 {
-	public VirtualKeyCode KeyKeycode;
-	public VirtualKeyCode KeyKeycodeModifier;
-	public VirtualKeyCode[] AlternateKeyKeycode;
+    public VirtualKeyCode KeyKeycode;
+    public VirtualKeyCode KeyKeycodeModifier;
+    public VirtualKeyCode[] AlternateKeyKeycode;
 
-	public void SimulateKeyPress()
-	{
-		InputSimulator.SimulateKeyPress(KeyKeycode);
-	}
+    public void SimulateKeyPress()
+    {
+        InputSimulator.SimulateKeyPress(KeyKeycode);
+    }
 
-	public void SimulateAlternateKeyPress()
-	{
-		InputSimulator.SimulateModifiedKeyStroke (KeyKeycodeModifier, AlternateKeyKeycode);
-	}
+    public void SimulateAlternateKeyPress()
+    {
+        InputSimulator.SimulateModifiedKeyStroke(KeyKeycodeModifier, AlternateKeyKeycode);
+    }
 }
