27e1a121818da6bf73163dc0998941e32a86965c
gdominikff <dominik.ganghofer@framefield.com>
2017-08-28 18:30:01 +0200

    added singleton from ff.utils

---
+++ Assets/vr_annotate/ff.utils/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace ff.utils
+{
+    /// <summary>
+    /// Singleton behaviour class, used for components that should only have one instance
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        /// <summary>
+        /// Returns the instance of the singleton
+        /// </summary>
+        public static T Instance { get; private set; }
+
+        /// <summary>
+        /// Returns whether the instance has been initialized or not.
+        /// </summary>
+        public static bool IsInitialized
+        {
+            get { return Instance != null; }
+        }
+
+        protected virtual void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogErrorFormat("Trying to instantiate a second instance of singleton class {0}", GetType().Name);
+            }
+            else
+            {
+                Instance = (T)this;
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+    }
+}
